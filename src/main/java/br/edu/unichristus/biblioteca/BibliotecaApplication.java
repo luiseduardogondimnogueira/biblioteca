53f98e867bd72f290611038d8e9588c714c03cb5
luiseduardogondimnogueira <159388314+luiseduardogondimnogueira@users.noreply.github.com>
2025-10-20 19:42:43 -0300

    Importação do Swagger

--- src/main/java/br/edu/unichristus/biblioteca/BibliotecaApplication.java
+++ src/main/java/br/edu/unichristus/biblioteca/BibliotecaApplication.java
@@ -1,7 +1,22 @@
 package br.edu.unichristus.biblioteca;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Documentação da API BIBLIOTECA",
+                version = "1.0",
+                description = "Trata-se da documentação da API construída para a avaliação da NP2",
+                contact = @Contact(
+                        name = "Luís Eduardo Nogueira, Cristiano Florêncio e Cauã Carvalho",
+                        email = "luiseduardogondimnogueira@gmail.com",
+                        url = "https://unichristus.edu.br")
+        )
+)
 
 @SpringBootApplication
 public class BibliotecaApplication {
